fea7c7c0e194abbf61f2300f9cb80e5e128b49d0
MD. Ismail Hosen James <md.ismailhosenismailjames@gmail.com>
2024-08-31 01:59:52 +0600

    Green themes added for elevated button

--- lib/src/language/translation.dart
+++ lib/src/language/translation.dart
@@ -1,4 +1,3 @@
-//AppTranslation.dart
 abstract class AppTranslation {
   static Map<String, Map<String, String>> translationsKeys = {
     "en": en,
@@ -22,97 +21,212 @@ abstract class AppTranslation {
 
 Map<String, String> en = {
   "Select a language": "Select a language",
-
+  "Next": "Next",
+  "Allow Background Location Access": "Allow Background Location Access",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "We must need location data for running this app with essential features. Please allow location access, then you can go next step.",
+  "Go to app settings and allow all the time location access":
+      "Go to app settings and allow all the time location access",
+  "Or": "Or",
+  "Setup Manually": "Setup Manually",
   //
 };
 
 Map<String, String> ar = {
   "Select a language": "اختر لغة",
-
+  "Next": "التالي",
+  "Allow Background Location Access": "السماح بالوصول إلى الموقع في الخلفية",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "نحتاج إلى بيانات الموقع لتشغيل هذا التطبيق باستخدام الميزات الأساسية. يرجى السماح بالوصول إلى الموقع، ثم يمكنك الانتقال إلى الخطوة التالية.",
+  "Go to app settings and allow all the time location access":
+      "انتقل إلى إعدادات التطبيق وقم بالسماح بالوصول إلى الموقع دائمًا",
+  "Or": "أو",
+  "Setup Manually": "إعداد يدوي",
   //
 };
 
 Map<String, String> id = {
   "Select a language": "Pilih bahasa",
-
+  "Next": "Selanjutnya",
+  "Allow Background Location Access": "Izinkan Akses Lokasi Latar Belakang",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Kami harus memiliki data lokasi untuk menjalankan aplikasi ini dengan fitur-fitur penting. Silakan izinkan akses lokasi, lalu Anda dapat melanjutkan ke langkah selanjutnya.",
+  "Go to app settings and allow all the time location access":
+      "Buka pengaturan aplikasi dan izinkan akses lokasi sepanjang waktu",
+  "Or": "Atau",
+  "Setup Manually": "Pengaturan Manual",
   //
 };
 
 Map<String, String> bn = {
   "Select a language": "একটি ভাষা নির্বাচন করুন",
-
+  "Next": "পরবর্তী",
+  "Allow Background Location Access":
+      "পটভূমি অবস্থান অ্যাক্সেস করার অনুমতি দিন",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "আমাদের অপরিহার্য বৈশিষ্ট্য সহ এই অ্যাপটি চালানোর জন্য অবস্থান ডেটা প্রয়োজন। অনুগ্রহ করে অবস্থান অ্যাক্সেস করার অনুমতি দিন, তারপরে আপনি পরবর্তী পদক্ষেপে যেতে পারেন।",
+  "Go to app settings and allow all the time location access":
+      "অ্যাপ সেটিংসে যান এবং সর্বদা অবস্থান অ্যাক্সেস করার অনুমতি দিন",
+  "Or": "অথবা",
+  "Setup Manually": "হাতে সেটআপ করুন",
   //
 };
 
 Map<String, String> hi = {
   "Select a language": "एक भाषा चुनें",
-
+  "Next": "अगला",
+  "Allow Background Location Access": "बैकग्राउंड लोकेशन एक्सेस की अनुमति दें",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "हमें इस ऐप को आवश्यक सुविधाओं के साथ चलाने के लिए स्थान डेटा की आवश्यकता होगी। कृपया स्थान एक्सेस की अनुमति दें, फिर आप अगले चरण में जा सकते हैं।",
+  "Go to app settings and allow all the time location access":
+      "ऐप सेटिंग्स में जाएं और हर समय लोकेशन एक्सेस की अनुमति दें",
+  "Or": "या",
+  "Setup Manually": "मैन्युअली सेटअप करें",
   //
 };
 
 Map<String, String> ur = {
   "Select a language": "ایک زبان منتخب کریں",
-
+  "Next": "اگلا",
+  "Allow Background Location Access": "بیک گراؤنڈ لوکیشن تک رسائی کی اجازت دیں",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "اس ایپ کو ضروری خصوصیات کے ساتھ چلانے کے لیے ہمیں مقام کے اعداد و شمار کی ضرورت ہوگی۔ براہ کرم مقام تک رسائی کی اجازت دیں، پھر آپ اگلے قدم پر جا سکتے ہیں۔",
+  "Go to app settings and allow all the time location access":
+      "ایپ کی ترتیبات پر جائیں اور تمام وقت مقام تک رسائی کی اجازت دیں",
+  "Or": "یا",
+  "Setup Manually": "مینیوالی سیٹ اپ کریں",
   //
 };
 
 Map<String, String> tr = {
   "Select a language": "Bir dil seçin",
-
+  "Next": "Sonraki",
+  "Allow Background Location Access": "Arka Plan Konum Erişimi'ne İzin Ver",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Bu uygulamayı temel özelliklerle çalıştırmak için konum verilerine ihtiyacımız var. Lütfen konum erişimine izin verin, ardından sonraki adıma geçebilirsiniz.",
+  "Go to app settings and allow all the time location access":
+      "Uygulama ayarlarına gidin ve her zaman konum erişimine izin verin",
+  "Or": "Veya",
+  "Setup Manually": "Manuel Yapılandırma",
   //
 };
 
 Map<String, String> fa = {
   "Select a language": "یک زبان را انتخاب کنید",
-
+  "Next": "بعدی",
+  "Allow Background Location Access": "اجازه دسترسی به مکان پس زمینه",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "برای اجرای این برنامه با ویژگی های ضروری، باید داده های موقعیت را داشته باشیم. لطفاً دسترسی به موقعیت را مجاز کنید، سپس می توانید به مرحله بعدی بروید.",
+  "Go to app settings and allow all the time location access":
+      "به تنظیمات برنامه بروید و همیشه اجازه دسترسی به موقعیت را بدهید",
+  "Or": "یا",
+  "Setup Manually": "تنظیم دستی",
   //
 };
 
 Map<String, String> pa = {
   "Select a language": "ਇਕ ਭਾਸ਼ਾ ਚੁਣੋ",
-
+  "Next": "ਅਗਲਾ",
+  "Allow Background Location Access": "ਬੈਕਗ੍ਰਾਉਂਡ ਸਥਾਨ ਪਹੁੰਚ ਦੀ ਇਜਾਜ਼ਤ ਦਿਓ",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "ਇਸ ਐਪ ਨੂੰ ਜ਼ਰੂਰੀ ਵਿਸ਼ੇਸ਼ਤਾਵਾਂ ਨਾਲ ਚਲਾਉਣ ਲਈ ਸਾਨੂੰ ਸਥਾਨ ਡੇਟਾ ਦੀ ਲੋੜ ਹੋਵੇਗੀ। ਕਿਰਪਾ ਕਰਕੇ ਸਥਾਨ ਪਹੁੰਚ ਦੀ ਇਜਾਜ਼ਤ ਦਿਓ, ਫਿਰ ਤੁਸੀਂ ਅਗਲੇ ਪੜਾਅ 'ਤੇ ਜਾ ਸਕਦੇ ਹੋ।",
+  "Go to app settings and allow all the time location access":
+      "ਐਪ ਸੈਟਿੰਗਜ਼ ਵਿੱਚ ਜਾਓ ਅਤੇ ਹਮੇਸ਼ਾ ਸਥਾਨ ਪਹੁੰਚ ਦੀ ਇਜਾਜ਼ਤ ਦਿਓ",
+  "Or": "ਅਤੇ / ਜਾਂ",
+  "Setup Manually": "ਮੈਨੂਅਲੀ ਸੈਟਅਪ ਕਰੋ",
   //
 };
 
 Map<String, String> ps = {
   "Select a language": "یوه ژبه وټاکئ",
-
+  "Next": "بله پسې",
+  "Allow Background Location Access": "پس لېږد ځای ته لاسرسی اجازه ورکړئ",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "دا اپلیکیشن د مهمو ځانګړتیاوو سره چلولو لپاره موږ ته ځای ډاټا ته اړتیا لرو. مهرباني وکړئ د ځای لاسرسی ته اجازه ورکړئ، بیا تاسو کولی شئ بل ګام ته لاړ شئ.",
+  "Go to app settings and allow all the time location access":
+      "د اپلیکیشن ترتیبونو ته لاړ شئ او هر وخت د ځای لاسرسی ته اجازه ورکړئ",
+  "Or": "یا",
+  "Setup Manually": "لاس لیکنه په لاس کړئ",
   //
 };
 
 Map<String, String> fr = {
   "Select a language": "Sélectionnez une langue",
-
+  "Next": "Suivant",
+  "Allow Background Location Access":
+      "Autoriser l'accès à la localisation en arrière-plan",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Nous avons besoin de données de localisation pour exécuter cette application avec les fonctionnalités essentielles. Veuillez autoriser l'accès à la localisation, puis vous pourrez passer à l'étape suivante.",
+  "Go to app settings and allow all the time location access":
+      "Accédez aux paramètres de l'application et autorisez l'accès à la localisation en permanence",
+  "Or": "Ou",
+  "Setup Manually": "Configuration manuelle",
   //
 };
 
 Map<String, String> sw = {
   "Select a language": "Chagua lugha",
-
+  "Next": "Ifuatayo",
+  "Allow Background Location Access": "Ruhusu Ufikiaji wa Mahali Pa Nyuma",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Tunahitaji data ya eneo ili kuendesha programu hii na vipengele muhimu. Tafadhali ruhusu upatikanaji wa eneo, kisha unaweza kwenda hatua inayofuata.",
+  "Go to app settings and allow all the time location access":
+      "Nenda kwenye mipangilio ya programu na uruhusu upatikanaji wa eneo kila wakati",
+  "Or": "Au",
+  "Setup Manually": "Sanidi Kibao",
   //
 };
 
 Map<String, String> ha = {
   "Select a language": "Zaɓi harshe",
-
+  "Next": "Na gaba",
+  "Allow Background Location Access":
+      "Ba da izinin Samun Dama zuwa Wuri a Bayan Fage",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Dole ne mu sami bayanan wuri don gudanar da wannan app tare da fasalolin da suka zama dole. Da fatan za a ba da izinin samun dama zuwa wuri, sannan za ku iya zuwa matakin na gaba.",
+  "Go to app settings and allow all the time location access":
+      "Je zuwa saitunan app ɗin kuma ba da izinin samun dama zuwa wuri a kowane lokaci",
+  "Or": "Ko",
+  "Setup Manually": "Sanya Aiki Da Hannu",
   //
 };
 
 Map<String, String> ms = {
   "Select a language": "Pilih bahasa",
-
+  "Next": "Seterusnya",
+  "Allow Background Location Access": "Izinkan Akses Lokasi Latar Belakang",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Kami perlu data lokasi untuk menjalankan aplikasi ini dengan fitur-fitur penting. Silakan izinkan akses lokasi, lalu Anda dapat melanjutkan ke langkah selanjutnya.",
+  "Go to app settings and allow all the time location access":
+      "Buka pengaturan aplikasi dan izinkan akses lokasi sepanjang waktu",
+  "Or": "Atau",
+  "Setup Manually": "Konfigurasi Manual",
   //
 };
 
 Map<String, String> ku = {
   "Select a language": "زارێک هەڵبژێرە",
-
+  "Next": "Piştî",
+  "Allow Background Location Access": "Destûra Bişandina Cihê Berîna Bidin",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Pêdivî ye ku em daneyên cihê ji bo bikaranîna vê appê bi taybetmendiyên bingehîn hebin. Ji kerema xwe destûr bidin ku bi cihê ve girêdayî bibin, hingê hûn dikarin biçin qonaxa din.",
+  "Go to app settings and allow all the time location access":
+      "Biçinên appê veke û destûr bidin ku her dem bi cihê ve girêdayî bibin",
+  "Or": "Anko",
+  "Setup Manually": "Destûra Bi Destan",
   //
 };
 
 Map<String, String> so = {
   "Select a language": "Dooro luqad",
-
+  "Next": "Xiga",
+  "Allow Background Location Access": "Oggolaasho Siinta Goobta Dhexe",
+  "We must need location data for running this app with essential features. Please allow location access, then you can go next step.":
+      "Waxaan u baahanahay xogta goobta si aan u maamulno app-kan oo leh astaamaha muhiimka ah. Fadlan oggolaasho siinta goobta, ka dibna waxaad u gudbi kartaa tallaabada xigta.",
+  "Go to app settings and allow all the time location access":
+      "U tagri app-ka oo oggolaasho siinta goobta mar walba",
+  "Or": "Ama",
+  "Setup Manually": "Diyaarinta Gacanta",
   //
 };
 
